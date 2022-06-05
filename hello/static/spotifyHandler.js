c5e77e41d231249ff8fbef4129541647aecd52c9
MarkooCZ <m.lohn@seznam.cz>
2022-06-05 15:25:25 +0200

    callback to spotify sing up

--- hello/static/spotifyHandler.js
+++ hello/static/spotifyHandler.js
@@ -1,5 +1,8 @@
 function spotifyLogin()
 {
-	var spotifyLoginWindow = window.open('https://accounts.spotify.com/authorize?client_id=clientID&redirect_uri=logged&response_type=code',
+	var clientID = "70b92749bfca4ddda75007ee40cc841d";
+	var redirectURL = "https://markoosplaylist/logged.herokuapp.com/"
+	var spotifyLoginWindow = window.open('https://accounts.spotify.com/authorize?client_id=' + clientID + 
+		'&redirect_uri=' + redirectURL + '&response_type=code',
 	 'Spotify Login', 'width=750, height=850');
 }
