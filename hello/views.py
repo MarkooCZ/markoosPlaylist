bf903147370d138e21670f5821d4e822e6891ec7
MarkooCZ <m.lohn@seznam.cz>
2022-06-06 17:46:35 +0200

    no content type header

--- hello/views.py
+++ hello/views.py
@@ -36,7 +36,7 @@ def loggedCallback(request):
 
     contentType = "application/x-www-form-urlencoded"
 
-    grantMeToken = requests.post(urlRequestToken, data=dataToPass, headers={"Authorization" : authorization, "Content-Type" : contentType})
+    grantMeToken = requests.post(urlRequestToken, data=dataToPass, headers={"Authorization" : authorization})
     grantMeToken = json.loads(grantMeToken.text)
     token = grantMeToken["access_token"]
 
