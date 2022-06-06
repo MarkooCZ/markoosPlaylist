f22d8bd89b7f99cb4dee43d2367afca967dc8641
MarkooCZ <m.lohn@seznam.cz>
2022-06-06 16:27:45 +0200

    changed from data parameter to form parameter

--- hello/views.py
+++ hello/views.py
@@ -36,7 +36,7 @@ def loggedCallback(request):
 
     contentType = "application/x-www-form-urlencoded"
 
-    grantMeToken = requests.post(urlRequestToken, data=dataToPass, headers={"Authorization" : authorization, "Content-Type" : contentType})
+    grantMeToken = requests.post(urlRequestToken, form=dataToPass, headers={"Authorization" : authorization, "Content-Type" : contentType})
     grantMeToken = json.loads(grantMeToken.text)
     token = grantMeToken["access_token"]
 
